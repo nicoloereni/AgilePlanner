7def5da448585f6a9ca0b7ad6055d9812eb8a260
nicolo <nicolo.ereni@trovaprezzi.it>
2015-10-20 17:18:27 +0200

    wip

--- app/src/main/java/com/nicoloereni/agileplanner/MainActivity.java
+++ app/src/main/java/com/nicoloereni/agileplanner/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import com.melnykov.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         tasksRecyclerView.setAdapter(new TasksAdapter());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.attachToRecyclerView(tasksRecyclerView);
+
+        getSupportActionBar().hide();
+
     }
 }
